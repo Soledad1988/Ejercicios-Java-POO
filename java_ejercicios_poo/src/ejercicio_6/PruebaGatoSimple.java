9ed52af3b265e8ad25a8d1f4459704807c24a7c5
Brenda <brentorres26@gmail.com>
2023-03-10 20:42:40 -0300

    se agregó un método al ejercicio 6

--- java_ejercicios_poo/src/ejercicio_6/PruebaGatoSimple.java
+++ java_ejercicios_poo/src/ejercicio_6/PruebaGatoSimple.java
@@ -10,5 +10,7 @@ public class PruebaGatoSimple {
 		tom.maulla();
 		System.out.println("Tienes habre?...veremos que hay de comer..");
 		tom.ronronea();
+		System.out.println("tarta de carne");
+		tom.come("tarta de carne");
 	}
 }
--- java_ejercicios_poo/src/ejercicio_6/gatoSimple.java
+++ java_ejercicios_poo/src/ejercicio_6/gatoSimple.java
@@ -32,5 +32,14 @@ public class gatoSimple {
 		System.out.println("mrrrrrr");
 	}
 
+	//hacer que el gato coma, a los gatos les gusta el pescado
+	//si es otro alimento lo rechaza
 	
+	public void come(String comida) {
+		if(comida.equals("pescado")) {
+			System.out.println("Hmmmmm, gracias!!");
+		}else {
+			System.out.println("Yo solo como pescado :)");
+		}
+	}
 }
